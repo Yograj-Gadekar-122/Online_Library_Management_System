8fff8708057224ebf098716c2e9927ba2a6f7c22
Yograj Gadekar <79786436+Yograj-Gadekar-122@users.noreply.github.com>
2021-09-30 15:36:34 +0530

    OLMS_AASY

---
+++ controller/footer.php
@@ -0,0 +1,17 @@
+
+<?php
+	
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<div class="footerContent">
+			<b>Library @ 2019-2020 | Contact Us: (A-A-S-Y Soft )</b>
+		</div>
+	</body>
+</html>
